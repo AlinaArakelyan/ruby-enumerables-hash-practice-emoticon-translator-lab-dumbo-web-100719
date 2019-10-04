29f2d052eca90ac6e39d9a7c2eb91d4e23862175
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-10-04 00:39:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,9 @@ emoticons_hash
 end 
 
 def get_japanese_emoticon(file_path, emoticon)
-  call = load_library(file_path)[:get_meaning]
+  call = load_library(file_path)[:get_emoticon][emoticon]
+  
+  result = load_library(path)[:get_emoticon][english_emoticon]
 end
 
 def get_english_meaning
