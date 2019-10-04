da67370fe25e8c5c3d17ef67b9d7cf68410769d3
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-10-04 00:53:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,5 +25,11 @@ end
 
 def get_english_meaning(file_path, emoticon)
    result = load_library(file_path)[:get_meaning][emoticon]
+   if result
+     return result
+     else
+    return "Sorry, that emoticon was not found"
+  end 
+     
    
 end
